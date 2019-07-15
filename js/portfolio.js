c053310777894fa3b7f7d0996bb139807a5cdadd
Jason Collum <jasoncollum@gmail.com>
2019-07-15 09:11:26 -0500

    add CSS to remove button highlight on focus and add jQuery to close dropdown when brand is clicked

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,6 +3,12 @@ $('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
 
+// Hide / Collapse menu bar when Nav Brand is clicked in Mobile View
+$('.navbar-brand').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+
 // Animate tech icons
 AOS.init({
     duration: 1200,
